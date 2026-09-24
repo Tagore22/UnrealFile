e386b4d8a2368eb2ca541b398287cf252b4db0db
홍지학 <5226947@hanmail.net>
2026-09-24 22:24:24 +0900

    FStreamableManager에 대하여

--- FStreamableManager.cpp
+++ FStreamableManager.cpp
@@ -4,6 +4,9 @@ LoadSynchronous()를 사용하여 필요할 때 메모리에 올리기 때문이다. 하지만 LoadSync
 로딩이 끝날 때까지 스레드가 대기한다는 단점이 있기에 FStreamableManager::RequestAsyncLoad()를 통한
 비동기 로딩으로 이어주는 것이 가장 베스트다.
 
+ 또한, FStreamableDelegate::CreateUObject() 일반적인 델리게이트와 달라서 비동기 로딩 이전에 액터가(this)
+삭제되었다면 콜백함수를 호출하지 않고 끝낸다.
+
 // 실제 리소스
 TSoftObjectPtr<UStaticMesh> WeaponMesh;
 
