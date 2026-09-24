255dca3053bd1f7ecc4caf7a0487842868da5e35
홍지학 <5226947@hanmail.net>
2026-09-24 22:59:49 +0900

    포인터지만 함수 호출시에는 템플릿 매개변수로 포인터가 아닌 타입을 넘기는 함수들

--- Pointer But Not Pointer.cpp
+++ Pointer But Not Pointer.cpp
@@ -4,5 +4,6 @@ SpawnActor<AWeaponBase>();
 NewObject<USkillBase>(this);
 GetSubsystem<UStatSubsystem>();
 FindRow<FSkillData>(RowName, Context);
+Ptr로 끝나는 스마트포인터류
 
 // 실제로는 포인터지만 넘기는 템플릿 매개변수는 포인터가 아니다.
