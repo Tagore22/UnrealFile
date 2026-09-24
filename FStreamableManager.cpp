bae3eb28e9a2663ddf3d93b18697c393a919377f
홍지학 <5226947@hanmail.net>
2026-09-24 22:56:48 +0900

    FStreamableManager에 대하여

--- FStreamableManager.cpp
+++ FStreamableManager.cpp
@@ -7,6 +7,8 @@ LoadSynchronous()를 사용하여 필요할 때 메모리에 올리기 때문이다. 하지만 LoadSync
  또한, FStreamableDelegate::CreateUObject() 일반적인 델리게이트와 달라서 비동기 로딩 이전에 액터가(this)
 삭제되었다면 콜백함수를 호출하지 않고 끝낸다.
 
+ 거기에 FStreamableHandle을 사용하여 비동기 로딩의 중복을 막을 수도 있다. 
+
 // 실제 리소스
 TSoftObjectPtr<UStaticMesh> WeaponMesh;
 
@@ -42,5 +44,66 @@ TSoftObjectPtr<UStaticMesh> WeaponMesh;
      }
 
      // 리소스값 대입
+     WeaponMeshComponent->SetStaticMesh(Mesh);
+ }
+
+ ////////
+
+ // WeaponBase.h
+
+ TSoftObjectPtr<UStaticMesh> WeaponMesh;
+
+ TSharedPtr<FStreamableHandle> WeaponLoadHandle;
+
+ void LoadWeaponMesh();
+ void OnWeaponMeshLoaded();
+
+ // WeaponBase.cpp
+
+ void AWeaponBase::LoadWeaponMesh()
+ {
+     // 이미 로딩된 경우
+     // 에셋(메시)를 확인한다
+     if (WeaponMesh.IsValid())
+     {
+         WeaponMeshComponent->SetStaticMesh(WeaponMesh.Get());
+         return;
+     }
+
+     // 이미 비동기 로딩 요청 중인 경우
+     // 핸들을 확인한다
+     if (WeaponLoadHandle.IsValid())
+     {
+         return;
+     }
+
+     FStreamableManager& StreamableManager =
+         UAssetManager::GetStreamableManager();
+
+     // 비동기 로딩 요청
+     // RequestAsyncLoad의 반환형이 TSharedPtr<FStreamableHandle>이기 때문에 반드시 TSharedPtr을 써야한다
+     WeaponLoadHandle = StreamableManager.RequestAsyncLoad(
+         WeaponMesh.ToSoftObjectPath(),
+         FStreamableDelegate::CreateUObject(
+             this,
+             &AWeaponBase::OnWeaponMeshLoaded
+         )
+     );
+ }
+
+ void AWeaponBase::OnWeaponMeshLoaded()
+ {
+     // 로딩 완료했으므로 Handle 참조 해제
+     // TSharedPtr의 Reset으로 포인터 해제
+     WeaponLoadHandle.Reset();
+
+     // 실제 UObject가 정상적으로 로드됐는지 확인
+     if (!WeaponMesh.IsValid())
+     {
+         return;
+     }
+
+     UStaticMesh* Mesh = WeaponMesh.Get();
+
      WeaponMeshComponent->SetStaticMesh(Mesh);
  }
